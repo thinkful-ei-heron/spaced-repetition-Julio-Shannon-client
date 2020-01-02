1ea4ed5990702683194a9015c0d5ed7726576928
julio hernandez <hernandezjulio58@ymail.com>
2020-01-02 09:46:24 -0800

    working on the client tests passing

--- src/components/LearningPage/LearningPage.js
+++ src/components/LearningPage/LearningPage.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './LearningPage.css';
 import Button from '../Button/Button';
 import UserContext from '../../contexts/UserContext';
+import LanguageApiService from '../../services/language-api-service';
 
 export default class LearningPage extends Component {
   state = {
     flipped: false,
     guess: null,
     answer: null,
-    //original: this.context.currentWord ? this.context.currentWord.nextWord : null,
-    //answer: this.context.answer ? this.context.answer : 'star',
+    currentResults: null,
   };
 
   static contextType = UserContext;
@@ -17,31 +17,33 @@ export default class LearningPage extends Component {
   handleClick = e => {
     e.preventDefault();
     let userInput = document.getElementById('learn-guess-input').value;
-    this.setState({
-      flipped: !this.state.flipped,
-      guess: userInput.trim(),
-    });
-    this.setState({
-      answer:
-        this.context.currentWord &&
-        userInput === this.context.currentWord.translation
-          ? 'right'
-          : 'wrong',
-    });
-    userInput = '';
+    if (this.state.flipped) {
+      document.getElementById('learn-guess-input').value = '';
+      LanguageApiService.fetchCurrentWord().then(res => {
+        this.context.setCurrentWord(res);
+        this.setState({ flipped: !this.state.flipped });
+      });
+    } else {
+      LanguageApiService.postListGuess({ guess: userInput.trim() }).then(
+        res => {
+          console.log(res);
+          this.context.setCurrentResult(res);
+          this.setState({
+            guess: userInput.trim(),
+            flipped: !this.state.flipped,
+          });
+        }
+      );
+    }
   };
 
   handleWrongAns = () => {
-    // this.setState({
-    //   answer: 'wrong'
-    // })
-    // this.handleFlip('wrong')
     return (
       <div className="Learn_Feedback">
         <h3>You Are Incorrect !</h3>
         <h4>
           The correct translation was{' '}
-          {this.context.currentWord && this.context.currentWord.translation}
+          {this.context.currentResult && this.context.currentResult.answer}
         </h4>
         <h4>You guessed {this.state.guess}</h4>
       </div>
@@ -49,35 +51,24 @@ export default class LearningPage extends Component {
   };
 
   handleRightAns = () => {
-    // this.setState({
-    //   answer: 'right'
-    // })
-    // this.handleFlip('right')
     return (
       <div className="Learn_Feedback">
         <h3>You Are Correct !</h3>
         <h4>
           The correct translation was{' '}
-          {this.context.currentWord && this.context.currentWord.translation}
+          {this.context.currentResult && this.context.currentResult.answer}
         </h4>
         <h4>You guessed {this.state.guess}</h4>
       </div>
     );
   };
 
-  // handleFlip = (string) => {
-  //   let element = document.getElementsByClassName('flip-card-back');
-  //   console.log(element);
-  //   console.log(element.length);
-  //   if(element.length !== 0){
-  //     element.classList.add(string);
-  //   }
-  // }
-
   render() {
     let flipCard = this.state.flipped ? 'flip-card flipped' : 'flip-card';
-    let flipCardBack = this.state.answer
-      ? `flip-card-back ${this.state.answer}`
+    let flipCardBack = this.context.currentResult
+      ? `flip-card-back ${
+          this.context.currentResult.isCorrect ? 'right' : 'wrong'
+        }`
       : 'flip-card-back';
     return (
       <>
@@ -106,28 +97,28 @@ export default class LearningPage extends Component {
               </div>
             </div>
             <div className={flipCardBack}>
-              {this.state.guess &&
-              this.state.guess === this.context.currentWord.translation
+              {this.context.currentResult &&
+              this.context.currentResult.isCorrect
                 ? this.handleRightAns()
                 : this.handleWrongAns()}
 
               <h3>Stats for this Word</h3>
               <p>
                 Correct{' '}
-                {this.context.currentWord &&
-                  this.context.currentWord.wordCorrectCount}{' '}
+                {this.context.currentResult &&
+                  this.context.currentResult.wordCorrectCount}{' '}
                 times
               </p>
               <p>
                 Incorrect{' '}
-                {this.context.currentWord &&
-                  this.context.currentWord.wordIncorrectCount}{' '}
+                {this.context.currentResult &&
+                  this.context.currentResult.wordIncorrectCount}{' '}
                 times
               </p>
-              <p>
+              <p className="DisplayScore">
                 Total Score:{' '}
-                {this.context.currentWord &&
-                  this.context.currentWord.totalScore}{' '}
+                {this.context.currentResult &&
+                  this.context.currentResult.totalScore}{' '}
               </p>
             </div>
           </div>
--- src/services/language-api-service.js
+++ src/services/language-api-service.js
@@ -8,17 +8,30 @@ const LanguageApiService = {
       headers: {
         authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
-    }).then(res => res.json())
+    }).then(res => res.json());
   },
-
   fetchCurrentWord() {
     return fetch(`${config.API_ENDPOINT}/language/head`, {
       method: 'GET',
       headers: {
         authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
-    }).then(res => !res.ok ? res.json().then(e => Promise.reject(e)) : res.json())
-  }
-}
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+  postListGuess(guess) {
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify(guess),
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+};
 
 export default LanguageApiService;
